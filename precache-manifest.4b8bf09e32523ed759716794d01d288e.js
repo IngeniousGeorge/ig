fa322e9fa22c8e84253899ea70b47f7158956b8a
ingeniousgeorge <ingeniousgeorge@gmail.com>
2019-12-18 11:15:52 +0100

    Updates

---
+++ precache-manifest.4b8bf09e32523ed759716794d01d288e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "318fd8c07494ee461b51ee6305acf3dd",
+    "url": "/ig/index.html"
+  },
+  {
+    "revision": "9b00a059c0484fd84b3c",
+    "url": "/ig/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e310de2562a8e9bbd6f2",
+    "url": "/ig/static/js/2.80e49b12.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ig/static/js/2.80e49b12.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9b00a059c0484fd84b3c",
+    "url": "/ig/static/js/main.667c16da.chunk.js"
+  },
+  {
+    "revision": "db1440aec945757c594c",
+    "url": "/ig/static/js/runtime-main.58a655de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ig/static/media/logo.5d5d9eef.svg"
+  }
+]);
